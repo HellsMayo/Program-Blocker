60c97cc7e45a69b3f320e7bc2ec633905c449055
Benjamin Hellmann <benjaminhellmann42@gmail.com>
2019-07-03 23:39:11 -0400

    Simplify path variables in global_constants.py

--- global_constants.py
+++ global_constants.py
@@ -2,5 +2,5 @@ from winreg import HKEY_CURRENT_USER
 
 default_hkey = HKEY_CURRENT_USER
 default_key = r'Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun'
-default_csv = r'C:\Users\Benjamin Hellmann\Documents\_Running Programs\File Blocker\blocked programs.csv'
-visible_caller_path = r"C:\Users\Benjamin Hellmann\Documents\_Running Programs\File Blocker\visible_caller.py"
+default_csv = r'blocked programs.csv'
+visible_caller_path = r'visible_caller.py'
